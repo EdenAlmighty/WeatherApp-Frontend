33114b64fcbd167ff0c62c24f0e9d2aabdf9aca2
EdenAlmighty <edengilady1@gmail.com>
2024-09-04 20:23:20 +0300

    Fix search suggestion location

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ForecastPreview from './ForecastPreview'
+import { utilService } from '../services/util.service'
 
 export default function WeatherForecast({ forecast }) {
     const now = new Date()
@@ -8,17 +9,19 @@ export default function WeatherForecast({ forecast }) {
     const startHour = Math.max(0, currentHour - 2)
     const endHour = Math.min(23, currentHour + 2)
 
-
     const filteredForecast = forecast.filter(item => {
-        const hour = parseInt(item.time.split(':')[0])
+        const hour = new Date(item.time * 1000).getHours()
         return hour >= startHour && hour <= endHour
     })
-    console.log('forecast: ', forecast);
-    
+
     return (
         <ul className="weather-forecast">
             {filteredForecast.map((item, idx) => (
-                <ForecastPreview key={idx} time={item.time} temp={item.temp} />
+                <ForecastPreview
+                    key={idx}
+                    time={utilService.formatTimestamp(item.time)}
+                    temp={item.temp}
+                />
             ))}
         </ul>
     )
